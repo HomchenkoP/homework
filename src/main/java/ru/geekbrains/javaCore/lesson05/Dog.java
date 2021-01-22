a3384d7c11978d08fb43324d274eab92f3efe3e2
HomchenkoP <homchenkop@yandex.ru>
2021-01-22 23:55:43 +0300

    закончил ДЗ №5

---
+++ src/main/java/ru/geekbrains/javaCore/lesson05/Dog.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.javaCore.lesson05;
+
+// Создать классы Собака и Кот с наследованием от класса Животное.
+// Все животные могут бежать и плыть. В качестве параметра каждому методу передается длина препятствия.
+// Результатом выполнения действия будет печать в консоль. (Например, dogBobik.run(150); -> 'Бобик пробежал 150 м.');
+// У каждого животного есть ограничения на действия (бег: кот 200 м., собака 500 м.; плавание: кот не умеет плавать, собака 10 м.).
+// Добавить подсчет созданных котов, собак и животных.
+
+public class Dog extends Animal {
+    private static int count = 0;
+    private String name;
+
+    public Dog (String name) {
+        super();
+        count++;
+        this.name = name;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    @Override
+    public void run(int howLong) {
+        if (howLong > 0 && howLong <= 500)
+            System.out.println("Собака "+name+" пробежала "+howLong+"м");
+        else if (howLong > 500)
+            System.out.println("Собака "+name+" пробежала только 500м и упала");
+    }
+
+    @Override
+    public void swim(int howLong) {
+        if (howLong > 0 && howLong <= 10)
+            System.out.println("Собака "+name+" проплыла "+howLong+"м");
+        else if (howLong > 10)
+            System.out.println("Собака "+name+" проплыла только 10м и утонула");
+    }
+}
